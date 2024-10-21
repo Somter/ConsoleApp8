5d742df2b3f738e4b7fc98ee03af6b92613f2d6c
Sasha <alecsand.unit@gmail.com>
2024-10-21 16:49:14 +0300

    Add project files.

---
+++ ConsoleApp8/ClassMenu.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class ClassMenu
+    {
+        ShapesArray shapesArray;
+
+        public ClassMenu()
+        {
+            shapesArray = new ShapesArray();
+        }
+
+        public void ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Добавить фигуру");
+                Console.WriteLine("2. Удалить фигуру");
+                Console.WriteLine("3. Печать всех фигур");
+                Console.WriteLine("4. Печать фигур указанного типа");
+                Console.WriteLine("5. Вычислить площадь всех фигур");
+                Console.WriteLine("6. Вычислить площадь фигур указанного типа");
+                Console.WriteLine("7. Сохранить фигуры в файл");
+                Console.WriteLine("8. Загрузить фигуры из файла");
+                Console.WriteLine("0. Выход");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddShape();
+                        break;
+                    case "2":
+                        RemoveShape();
+                        break;
+                    case "3":
+                        shapesArray.Print();
+                        break;
+                    case "4":
+                        OutputSpecifiedFigure();
+                        break;
+                    case "5":
+                        shapesArray.AreaFigure();
+                        break;
+                    case "6":
+                        AreaTypeFigure();
+                        break;
+                    case "7":
+                        shapesArray.Save();
+                        break;
+                    case "8":
+                        shapesArray.Load();
+                        break;
+                    case "0":
+                        Console.WriteLine("Выход из программы.");
+                        return; 
+                    default:
+                        Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+
+        private void AddShape()
+        {
+            Console.WriteLine("Выберите тип фигуры для добавления (Triangle, Rectangle, Circle):");
+            string shapeType = Console.ReadLine();
+
+            Shape shape = null;
+
+            if (shapeType.Equals("Triangle", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("Введите длины катетов (a b): ");
+                string[] parts = Console.ReadLine().Split(' ');
+                double cathetus1 = double.Parse(parts[0]);
+                double cathetus2 = double.Parse(parts[1]);
+                shape = new Triangle(cathetus1, cathetus2);
+            }
+            else if (shapeType.Equals("Rectangle", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("Введите координаты левого верхнего и правого нижнего углов (x1 y1 x2 y2): ");
+                string[] parts = Console.ReadLine().Split(' ');
+                double x1 = double.Parse(parts[0]);
+                double y1 = double.Parse(parts[1]);
+                double x2 = double.Parse(parts[2]);
+                double y2 = double.Parse(parts[3]);
+                shape = new Rectangle(x1, y1, x2, y2);
+            }
+            else if (shapeType.Equals("Circle", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("Введите координаты центра и радиус (x y radius): ");
+                string[] parts = Console.ReadLine().Split(' ');
+                double x = double.Parse(parts[0]);
+                double y = double.Parse(parts[1]);
+                double radius = double.Parse(parts[2]);
+                shape = new Circle(x, y, radius);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный тип фигуры.");
+                return;
+            }
+
+            shapesArray.Add(shape);
+            Console.WriteLine($"{shapeType} добавлена.");
+        }
+
+        private void RemoveShape()
+        {
+            Console.WriteLine("Введите тип фигуры для удаления (Triangle, Rectangle, Circle):");
+            string shapeType = Console.ReadLine();
+
+            Shape shapeToRemove = null;
+
+            for (int i = 0; i < shapesArray.Size; i++)
+            {
+                Shape shape = shapesArray[i];
+                if (shapeType.Equals("Triangle", StringComparison.OrdinalIgnoreCase) && shape is Triangle)
+                {
+                    shapeToRemove = shape;
+                    break;
+                }
+                else if (shapeType.Equals("Rectangle", StringComparison.OrdinalIgnoreCase) && shape is Rectangle)
+                {
+                    shapeToRemove = shape;
+                    break;
+                }
+                else if (shapeType.Equals("Circle", StringComparison.OrdinalIgnoreCase) && shape is Circle)
+                {
+                    shapeToRemove = shape;
+                    break;
+                }
+            }
+
+            if (shapeToRemove != null)
+            {
+                shapesArray.Remove(shapeToRemove);
+                Console.WriteLine($"{shapeType} удалена.");
+            }
+            else
+            {
+                Console.WriteLine("Фигура не найдена.");
+            }
+        }
+
+        private void OutputSpecifiedFigure()
+        {
+            Console.WriteLine("Введите тип фигуры для вывода (Triangle, Rectangle, Circle):");
+            string figure = Console.ReadLine();
+            shapesArray.OutputSpecifiedFigure(figure);
+        }
+
+        private void AreaTypeFigure()
+        {
+            Console.WriteLine("Введите тип фигуры для вычисления площади (Triangle, Rectangle, Circle):");
+            string figure = Console.ReadLine();
+            shapesArray.AreaTypeFigure(figure);
+        }
+    }
+}
---
+++ ConsoleApp8/ShapesArray.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class ShapesArray
+    {
+        Shape[] shapes;
+        int size;
+
+        public ShapesArray() 
+        { 
+            shapes = new Shape[2];
+            size = 0;
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public Shape this[int index] // Это индексатор для доступа к фигурам в классе Menu
+        {
+            get
+            {
+                if (index < 0 || index >= size)
+                {
+                    throw new IndexOutOfRangeException("Индекс вне диапазона массива.");
+                }
+                return shapes[index];
+            }
+        }
+
+        // добавлем фигуру
+        public void Add(Shape shape) 
+        {
+            if (size == shapes.Length)
+            {
+                Shape[] newShapes = new Shape[shapes.Length * 2];
+                for (int i = 0; i < shapes.Length; i++)
+                {
+                    newShapes[i] = shapes[i];
+                }
+                shapes = newShapes;
+            }
+
+            shapes[size] = shape;
+            size++;
+        }
+
+        //удаляем фигуру
+        public void Remove(Shape shape)
+        {
+            int index = Array.IndexOf(shapes, shape, 0, size); // получаем индекс фигуры которую нужно удалить
+            if (index >= 0)
+            {
+               
+                for (int i = index; i < size - 1; i++)
+                {
+                    shapes[i] = shapes[i + 1];
+                }
+                shapes[size - 1] = null;
+                size--;
+                Console.WriteLine("");
+            }
+            
+        }
+
+        // печатем все фигуры
+        public void Print() 
+        {
+            for (int i = 0; i < size; i++) 
+            {
+                shapes[i].Show();
+            }
+        }
+
+        // Выводим фигуры указаного типа
+        public void OutputSpecifiedFigure(string figure) 
+        {
+            bool figureFound = false; 
+            for (int i = 0; i < size; i++)
+            {
+                Shape shape = shapes[i];
+                if (figure == "Triangle" && shape is Triangle)
+                {
+                    shape.Show();
+                    figureFound = true; 
+                }
+                else if (figure == "Rectangle" && shape is Rectangle)
+                {
+                    shape.Show();
+                    figureFound = true;
+                }
+                else if (figure == "Circle" && shape is Circle)
+                {
+                    shape.Show();
+                    figureFound = true;
+                }
+            }
+
+            if (!figureFound)
+            {
+                Console.WriteLine("Такой фигуры нет.");
+            }
+        }
+
+        // вычисляем плозадь фигуры
+        public void AreaFigure() 
+        {
+            Console.WriteLine("");
+            double area;
+            for (int i = 0; i < size; i++)
+            {
+                area = shapes[i].Area();
+                Console.WriteLine("Плоащадь " + i + " фигуры: " + area);  
+            }
+        }
+
+        // вычисляем площадь фигуры указанного типа
+        public void AreaTypeFigure(string figure) 
+        {
+            Console.WriteLine("");
+            double area;    
+            bool figureFound = false;
+            for (int i = 0; i < size; i++)
+            {
+                Shape shape = shapes[i];
+                if (figure == "Triangle" && shape is Triangle)
+                {
+                    area = shape.Area();
+                    Console.WriteLine("Плоащадь треугольника: " + area);
+                    figureFound = true;
+                }
+                else if (figure == "Rectangle" && shape is Rectangle)
+                {
+                    area = shape.Area();
+                    Console.WriteLine("Плоащадь прямоугольника: " + area);
+                    figureFound = true;
+                }
+                else if (figure == "Circle" && shape is Circle)
+                {
+                    area = shape.Area();
+                    Console.WriteLine("Плоащадь круга: " + area);
+                    figureFound = true;
+                }
+            }
+
+            if (!figureFound)
+            {
+                Console.WriteLine("Такой фигуры нет.");
+            }
+        }
+
+        // сохраняем фигуры в файл
+        public void Save() 
+        {
+            using (StreamWriter writer = new StreamWriter("Figure.txt")) 
+            {
+                for (int i =0;i < size; i++) 
+                {
+                    shapes[i].Save(writer);                       
+                }
+            }
+            Console.WriteLine("Фигуры сохранены в файл");
+        }
+
+        // загружаем фигурі из файла
+        public void Load() 
+        {
+            size = 0;
+
+            using (StreamReader read = new StreamReader("Figure.txt")) 
+            {
+                while (!read.EndOfStream)
+                {
+                    string shapeType = read.ReadLine();
+                    Shape shape = null;
+
+                    if (shapeType == "Triangle")
+                    {
+                        shape = new Triangle(0, 0);
+                    }
+                    else if (shapeType == "Rectangle")
+                    {
+                        shape = new Rectangle(0, 0, 0, 0);
+                    }
+                    else if (shapeType == "Circle")
+                    {
+                        shape = new Circle(0, 0, 0);
+                    }
+
+                    if (shape != null)
+                    {
+                        shape.Load(read); 
+                        Add(shape);   
+                    }
+                }
+            }
+            Console.WriteLine("Фигуры загружены из файла");
+        }
+    }
+
+
+}
